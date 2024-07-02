5422d101a5b4dc4046a67d5b6a97b0a090619b30
ernest75 <ernestschneider@gmail.com>
2024-07-02 16:50:39 +0200

    Introduced junit 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,16 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    testOptions.unitTests {
+        isReturnDefaultValues = true
+        all { tests ->
+            tests.useJUnitPlatform()
+            tests.testLogging {
+                events("passed", "failed", "skipped")
+            }
+        }
+    }
 }
 
 dependencies {
---
+++ app/src/test/java/com/ernestschcneider/remindersapp/TestDoubles.kt
@@ -0,0 +1,13 @@
+package com.ernestschcneider.remindersapp
+
+import com.google.common.truth.Truth
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class TestDoubles {
+    @Test
+    fun tryout() {
+        val result = "test"
+        Truth.assertThat(result).isEqualTo("test")
+    }
+}
