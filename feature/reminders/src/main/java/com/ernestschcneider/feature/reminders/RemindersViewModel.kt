f8a23ee07908590a161e6609806a1b8f10a6125d
ernest75 <ernestschneider@gmail.com>
2024-08-01 19:44:02 +0200

    Improved items creation

--- feature/reminders/src/main/java/com/ernestschcneider/feature/reminders/RemindersViewModel.kt
+++ feature/reminders/src/main/java/com/ernestschcneider/feature/reminders/RemindersViewModel.kt
@@ -6,66 +6,23 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class RemindersViewModel: ViewModel() {
+class RemindersViewModel : ViewModel() {
     private val _screenState = MutableStateFlow(RemindersScreenState())
     val screenState: StateFlow<RemindersScreenState> = _screenState.asStateFlow()
-    val reminderNote = Reminder(
-        title = "GGGGGGGGGG",
-        type = ReminderType.Note,
-        id = "1"
-    )
-    val reminderList = Reminder(
-        title = "ddd",
-        type = ReminderType.List,
-        id = "2"
-    )
-    val reminderNote2 = Reminder(
-        title = "dfd",
-        type = ReminderType.Note,
-        id = "1"
-    )
-    val reminderList2 = Reminder(
-        title = "G",
-        type = ReminderType.List,
-        id = "2"
-    )
-    val reminderNote3 = Reminder(
-        title = "GGGGGGGGGG",
-        type = ReminderType.Note,
-        id = "1"
-    )
-    val reminderList3 = Reminder(
-        title = "ddd",
-        type = ReminderType.List,
-        id = "2"
-    )
-    val reminderNote4 = Reminder(
-        title = "dfd",
-        type = ReminderType.Note,
-        id = "1"
-    )
-    val reminderList4 = Reminder(
-        title = "G",
-        type = ReminderType.List,
-        id = "2"
-    )
-    val items = listOf(
-        reminderNote,
-        reminderList,
-        reminderList2,
-        reminderList3,
-        reminderNote2,
-        reminderNote3,
-        reminderNote4,
-        reminderList4
-    )
+    var items = (1..50).map {
+        Reminder(
+            id = it.toString(), title = "Item $it", type = if (it % 2 == 0) {
+                ReminderType.Note
+            } else ReminderType.List
+        )
+    }
 
     fun loadRemindersDetails() {
         _screenState.update { it.copy(reminders = items) }
     }
 
     fun removeItem(item: Reminder) {
-        val items = items.filter { it != item }.toList()
+        items = items.filter { it != item }
         _screenState.update { it.copy(reminders = items) }
     }
 }
