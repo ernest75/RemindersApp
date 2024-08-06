408809526f1ad490d124f078e81a2097cce2af66
ernestschneider <ernestschneider@gmail.com>
2024-08-06 10:49:50 +0200

    Added domain module

---
+++ domain/local/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.kapt)
+}
+
+android {
+    namespace = "com.ernestschcneider.remindersapp.local"
+    compileSdk = libs.versions.compileSdkVersion.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdkVersion.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        val javaVersion = libs.versions.javaVersion.get()
+        sourceCompatibility = JavaVersion.toVersion(javaVersion)
+        targetCompatibility = JavaVersion.toVersion(javaVersion)
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.javaVersion.get()
+    }
+}
+
+dependencies {
+    implementation(project(":core:database"))
+    implementation(libs.bundles.hilt)
+
+    kapt(libs.hilt.compiler)
+}
