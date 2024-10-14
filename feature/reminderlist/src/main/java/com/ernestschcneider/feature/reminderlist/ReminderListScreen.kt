f3387eb95298d82aea9ae7499d0bcfeb572c8baf
ernest75 <ernestschneider@gmail.com>
2024-10-14 15:27:58 +0200

    Updated imports

--- feature/reminderlist/src/main/java/com/ernestschcneider/feature/reminderlist/ReminderListScreen.kt
+++ feature/reminderlist/src/main/java/com/ernestschcneider/feature/reminderlist/ReminderListScreen.kt
@@ -10,10 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
